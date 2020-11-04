564414a0facb7687b29d68dfcd79c155637f16c6
Hidayatul Mukhlisin <hidayatulmukhlisin069@gmail.com>
2020-11-04 19:00:29 +0700

    home page
    sudah 2 tahap blm sampai login

--- application/views/home_page.php
+++ application/views/home_page.php
@@ -4,9 +4,10 @@
 		<div class="row">
 			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
 				<div class="hero-left">
+					<div class="cover"></div>
 					<div class="wrapper">
 						<h1 class="hots-head no-transform mb10">
-							Welcome to <span class="main-color">HOTs</span>
+							Welcome to <span class="main-color">School</span>
 						</h1>
 						<h4 class="hots-category mb20">
 							<span class="dash"></span> Higher Order Thinking Skills
@@ -26,3 +27,30 @@
 		</div>
 	</div>
 </section>
+<!-- About Us -->
+<section class="section-intermezo">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<div class="intermezo-image"></div>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+				<div class="intermezo-word text-right">
+					<div class="wrapper">
+						<h1 class="hots-head no-transform mb20">
+							About <span class="main-color">School</span>
+						</h1>
+						<!-- <div class="divider"></div> -->
+						<p class="hots-category no-transform info">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.
+						</p>
+						<p class="hots-category no-transform info">
+							Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- End About Us -->
